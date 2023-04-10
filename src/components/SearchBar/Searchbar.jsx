4f1053230608d92d5db00239e4a9b0f7551794cc
Matthew <78713317+mreed4@users.noreply.github.com>
2023-04-09 21:27:25 -0700

    Build out renderSortByOptions function

--- src/components/SearchBar/Searchbar.jsx
+++ src/components/SearchBar/Searchbar.jsx
@@ -5,3 +5,12 @@ const sortByOptions = {
   "Highest Rated": "rating",
   "Most Reviewed": "review_count",
 };
+
+export default function SearchBar() {
+  function renderSortByOptions() {
+    return Object.keys(sortByOptions).map((sortByOption) => {
+      let sortByOptionValue = sortByOptions[sortByOption];
+      return <li key={sortByOptionValue}>{sortByOption}</li>;
+    });
+  }
+}
