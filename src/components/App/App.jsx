d5ff192d355d7873d426080012acea77c9c7a2de
Matthew <78713317+mreed4@users.noreply.github.com>
2023-04-10 20:31:52 -0700

    Begin Part 2 of project

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,18 @@ import "./App.css";
 import BusinessList from "../BusinessList/BusinessList";
 import SearchBar from "../SearchBar/SearchBar";
 
+const business = {
+  imageSrc: "https://content.codecademy.com/programs/react/ravenous/pizza.jpg",
+  name: "MarginOtto Pizzeria",
+  address: "1010 Paddington Way",
+  city: "Flavortown",
+  state: "NY",
+  zipCode: "10101",
+  category: "Italian",
+  rating: 4.5,
+  reviewCount: 90,
+};
+
 function App() {
   return (
     <div className="App">
