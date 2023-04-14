1391d899d86746ef2e1894cb509441dc287c5581
Matthew <78713317+mreed4@users.noreply.github.com>
2023-04-13 20:51:43 -0700

    Conditional items

--- src/components/BusinessList/BusinessList.jsx
+++ src/components/BusinessList/BusinessList.jsx
@@ -7,11 +7,13 @@ import Business from "../Business/Business";
 export default function BusinessList() {
   const { businesses } = useContext(AppContext);
 
-  return (
+  return businesses.length >= 1 ? (
     <div className="BusinessList">
       {businesses.map((business) => {
         return <Business key={business.alias} business={business} />;
       })}
     </div>
+  ) : (
+    <div className="BusinessList">First you need to find a business</div>
   );
 }
