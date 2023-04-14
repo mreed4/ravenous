4f94db162a2f97ae4d03786ae5718fcc133f1fd3
Matthew <78713317+mreed4@users.noreply.github.com>
2023-04-13 20:35:21 -0700

    Make this a form

--- src/components/SearchBar/Searchbar.jsx
+++ src/components/SearchBar/Searchbar.jsx
@@ -8,31 +8,35 @@ export default function SearchBar() {
 
   return (
     <div className="SearchBar">
-      <div className="SearchBar-sort-options">
-        <ul>
-          {
-            /* Previously this was a call to a function from context */
-            Object.keys(sortByOptions).map((sortByOption) => {
-              let sortByOptionValue = sortByOptions[sortByOption];
-              return (
-                <li
-                  key={sortByOptionValue}
-                  className={getSortByClass(sortByOptionValue)}
-                  onClick={() => handleSortByChange(sortByOptionValue)}>
-                  {sortByOption}
-                </li>
-              );
-            })
-          }
-        </ul>
-      </div>
-      <div className="SearchBar-fields">
-        <input value={term} onChange={handleTermChange} placeholder="Search Businesses" />
-        <input value={location} onChange={handleLocationChange} placeholder="Where?" />
-      </div>
-      <div className="SearchBar-submit">
-        <a onClick={handleSearch}>Let's Go</a>
-      </div>
+      <form onSubmit={handleSearch}>
+        <div className="SearchBar-sort-options">
+          <ul>
+            {
+              /* Previously this was a call to a function from context */
+              Object.keys(sortByOptions).map((sortByOption) => {
+                let sortByOptionValue = sortByOptions[sortByOption];
+                return (
+                  <li
+                    key={sortByOptionValue}
+                    className={getSortByClass(sortByOptionValue)}
+                    onClick={() => handleSortByChange(sortByOptionValue)}>
+                    {sortByOption}
+                  </li>
+                );
+              })
+            }
+          </ul>
+        </div>
+        <div className="SearchBar-fields">
+          <input value={term} onChange={handleTermChange} placeholder="Search Businesses" />
+          <input value={location} onChange={handleLocationChange} placeholder="Where?" />
+        </div>
+        <div className="SearchBar-submit">
+          <button type="submit" onClick={handleSearch}>
+            Let's Go
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
