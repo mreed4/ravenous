3f7da8e6b93ffd3662208e8b5484d3b7f4375435
Matthew <78713317+mreed4@users.noreply.github.com>
2023-04-13 21:28:42 -0700

    Specificity

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <AppProvider>
       <div className="App">
-        <h1>ravenous</h1>
+        <h1 className="site-name">ravenous</h1>
         <SearchBar />
         <BusinessList />
       </div>
